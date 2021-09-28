77a0d499d6f3ddca212805e2d3b3797579bb89db
JackFram <erdos_zzh@163.com>
2021-09-27 23:27:07 -0400

    v0.002

--- eval/__init__.py
+++ eval/__init__.py
@@ -1,7 +1,1 @@
-net = load_state_dict(path)
-net_interpreter = ResNetInt(net)
-train_batch = get_dataloader
-feature_list, name_list, out = net_interpreter(train_batch)
-
-visualize(feature_list, name_list)
 
