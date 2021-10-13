34f296923476480a445fd322d0c22cf7f45d61d2
zhc180 <zhc180@ucsd.edu>
2021-10-13 17:39:01 -0400

    Update pruning

--- approx_optim/pruning.py
+++ approx_optim/pruning.py
@@ -12,18 +12,26 @@ class PruningOp(BaseOp):
         self.op_name = "pruning"
         self.amount = amount
         self.method = method
-        # self.prunable()
 
-    def apply(self, name):
+    def apply(self, name_list, verbose=False, *args, **kwargs):
+        name_set = set()
+        for name in name_list:
+            if name not in self.operatable:
+                print("{} is not a operatable layer, retry something in:{} !".format(name, self.operatable))
+                raise AttributeError
+            name_set.add(name)
+            
         model_to_prune = copy.deepcopy(self.model)
-        module_name = name.replace("_", ".")
-        print(module_name)
-        for name, mod in model_to_prune.named_modules():
-            if name == module_name:
-                print(list(mod.named_parameters()))
+        for mod_name, mod in model_to_prune.named_modules():
+            if mod_name in name_set:
+                if verbose:
+                    print(f"Module weights before pruning: {list(mod.named_parameters())}")
                 self._prune(mod)
-                print(list(mod.named_parameters()))
+                if verbose:
+                    print(f"Module weights after pruning: {list(mod.named_parameters())}")
         self.mod_model = model_to_prune
+
+        return self.mod_model
 
 
     def _prune(self, module:nn.Module):
@@ -41,3 +49,4 @@ class PruningOp(BaseOp):
         for name, mod in self.model.named_modules():
             if isinstance(mod, nn.Linear) or isinstance(mod, nn.Conv2d):
                 ret_list.append(name)
+        return ret_list
