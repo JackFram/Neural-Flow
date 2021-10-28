f83591d0bae9c281c784b45d68c0981f29040933
JackFram <zzh_jackfram@outlook.com>
2021-10-28 20:10:42 +0000

    imagenet16120 dataloader ready

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,9 +1,9 @@
 from .cv_dataloader import *
 
 
-def get_dataset(name: str, root: str = '../data/ImageNet16'):
+def get_dataset(name: str, root: str = '../data'):
     print('==> Preparing data..')
     if name == "cifar10":
         return Cifar10(root)
     if name == "imagenet16":
-        return ImageNet(root)
+        return ImageNet16(os.path.join(root, "ImageNet16"))
--- dataset/cv_dataloader.py
+++ dataset/cv_dataloader.py
@@ -1,6 +1,7 @@
 import torchvision
 import torchvision.transforms as transforms
 import torch
+from dataset.imagenet_16 import ImageNet16Data
 
 import os
 import argparse
@@ -40,7 +41,7 @@ class Cifar10:
         return self.testloader
 
 
-class ImageNet:
+class ImageNet16:
     def __init__(self, root_dir="./data"):
         self.transform_train = transforms.Compose([
             transforms.RandomSizedCrop(224),
@@ -56,13 +57,13 @@ class ImageNet:
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
 
-        self.trainset = torchvision.datasets.ImageFolder(
-            root=root_dir, transform=self.transform_train)
+        self.trainset = ImageNet16Data(
+            root=root_dir, train=True, transform=self.transform_train)
         self.trainloader = torch.utils.data.DataLoader(
             self.trainset, batch_size=128, shuffle=True, num_workers=2)
 
-        self.testset = torchvision.datasets.ImageFolder(
-            root=root_dir, transform=self.transform_test)
+        self.testset = ImageNet16Data(
+            root=root_dir, train=False, transform=self.transform_test)
         self.testloader = torch.utils.data.DataLoader(
             self.testset, batch_size=100, shuffle=False, num_workers=2)
 
